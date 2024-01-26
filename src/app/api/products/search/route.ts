82e0f4ffc4e60b39be9f1ba7a64ffd0875cc21be
viniciussgp <viniciusalvessgp@hotmail.com>
2024-01-26 16:55:11 -0300

    feat: page search products

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+import data from '../data.json'
+import type { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get('q'))
+
+  const products = data.products.filter((product) => {
+    return product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  })
+
+  return Response.json(products)
+}
